4c40066e37c0ce4adc637171ed798203c4fb2dc8
Ox Cart <ox.to.a.cart@gmail.com>
2014-10-29 13:50:24 -0500

    Added original natty binaries and script for embedding them

---
+++ embednatty.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+###############################################################################
+#
+# This script regenerates the source files that embed the natty executable.
+#
+###############################################################################
+
+go-bindata -nomemcopy -nocompress -pkg natty -prefix binaries/osx -o natty/natty_darwin.go binaries/osx
+go-bindata -nomemcopy -nocompress -pkg natty -prefix binaries/linux_386 -o natty/natty_linux_386.go binaries/linux_386
+go-bindata -nomemcopy -nocompress -pkg natty -prefix binaries/linux_amd64 -o natty/natty_linux_amd64.go binaries/linux_amd64
