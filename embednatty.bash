df1a10fe039c12578757f9b7c4d6b8364c5a4360
Ox Cart <ox.to.a.cart@gmail.com>
2014-10-30 14:50:54 -0500

    Added windows binary (currently only works on 64-bit windows)

--- embednatty.bash
+++ embednatty.bash
@@ -9,3 +9,4 @@
 go-bindata -nomemcopy -nocompress -pkg natty -prefix binaries/osx -o natty/natty_darwin.go binaries/osx
 go-bindata -nomemcopy -nocompress -pkg natty -prefix binaries/linux_386 -o natty/natty_linux_386.go binaries/linux_386
 go-bindata -nomemcopy -nocompress -pkg natty -prefix binaries/linux_amd64 -o natty/natty_linux_amd64.go binaries/linux_amd64
+go-bindata -nomemcopy -nocompress -pkg natty -prefix binaries/windows -o natty/natty_windows.go binaries/windows
